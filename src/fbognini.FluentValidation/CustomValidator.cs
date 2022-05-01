0b197c11a654d45e0b0a63c85aac88a9fae8396b
Francesco Bognini <info@francescobognini.com>
2022-05-01 19:49:46 +0200

    first commit

---
+++ src/fbognini.FluentValidation/CustomValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fbognini.FluentValidation
+{
+    public class CustomValidator<T> : AbstractValidator<T>
+    {
+        public override ValidationResult Validate(ValidationContext<T> context)
+        {
+            var validationResult = base.Validate(context);
+            if (!validationResult.IsValid)
+            {
+                var failures = validationResult.Errors.ToList();
+                if (failures.Count != 0)
+                    throw new Exceptions.ValidationException(failures);
+            }
+
+            return validationResult;
+        }
+    }
+}
---
+++ src/fbognini.FluentValidation/ExtensionMethods.cs
@@ -0,0 +1,39 @@
+﻿using FluentValidation;
+using System;
+using System.Linq;
+
+namespace fbognini.FluentValidation
+{
+    public static class ExtensionsMethods
+    {
+        public static string ToStringWithNull<TProperty>(this TProperty c)
+        {
+            if (c == null)
+                return "null";
+
+            return c.ToString();
+        }
+
+        public static IRuleBuilderOptions<T, TProperty> In<T, TProperty>(this IRuleBuilder<T, TProperty> ruleBuilder, params TProperty[] validOptions)
+        {
+            string formatted;
+            if (validOptions == null || validOptions.Length == 0)
+            {
+                throw new ArgumentException("At least one valid option is expected", nameof(validOptions));
+            }
+            else if (validOptions.Length == 1)
+            {
+                formatted = validOptions[0].ToStringWithNull();
+            }
+            else
+            {
+                // format like: option1, option2 or option3
+                formatted = $"{string.Join(", ", validOptions.Select(vo => vo.ToStringWithNull()).ToArray(), 0, validOptions.Length - 1)} or {validOptions.Last()}";
+            }
+
+            return ruleBuilder
+                .Must(validOptions.Contains)
+                .WithMessage($"{{PropertyName}} must be one of these values: {formatted}");
+        }
+    }
+}
